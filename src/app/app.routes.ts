37ff5680d606a935e03594040b76d37726d84bbf
HasiukI <136508423+HasiukI@users.noreply.github.com>
2025-02-02 17:39:52 +0200

    cabinet

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { MainComponent } from './views/main/main.component';
 import { LoginRegisterComponent } from './views/login-register/login-register.component';
+import { CabinetComponent } from './views/cabinet/cabinet.component';
 
 export const routes: Routes = [
     { path: '' , component:MainComponent },
-    { path:'login-registration/:mode', component:LoginRegisterComponent}
+    { path:'login-registration/:mode', component:LoginRegisterComponent},
+    { path:'cabinet', component:CabinetComponent}
 ];
