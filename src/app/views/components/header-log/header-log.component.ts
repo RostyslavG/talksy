2298664cc2cd3aca07ae2730e36b8105b4f43056
HasiukI <136508423+HasiukI@users.noreply.github.com>
2025-02-02 20:07:14 +0200

    add seting

---
+++ src/app/views/components/header-log/header-log.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-header-log',
+  standalone: true,
+  imports: [],
+  templateUrl: './header-log.component.html',
+  styleUrl: './header-log.component.css'
+})
+export class HeaderLogComponent {
+
+}
