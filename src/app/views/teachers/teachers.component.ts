e17bf61e078b783ea81942fa77434118b4bed673
HasiukI <136508423+HasiukI@users.noreply.github.com>
2025-04-13 12:54:57 +0300

    uoi

--- src/app/views/teachers/teachers.component.ts
+++ src/app/views/teachers/teachers.component.ts
@@ -65,6 +65,7 @@ export class TeachersComponent implements OnInit {
         }
     }
     
+    
     selectLevel(level: string): void {
         this.teacherForm.patchValue({ level: level });
     }
